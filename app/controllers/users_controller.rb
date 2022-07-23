31af06071d3ab4f98459100b2df4523f0d804dd7
Iulian <ilates@yahoo.com>
2022-07-23 19:26:58 +0300

    am adaugat metoda login si ruta apoi am rulat aceasta logare in Postman

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,6 +52,17 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
+  def login
+    @user = User.find_by(email: user_params[:email])
+    if @user && @user.authenticate(user_params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}, status: :ok
+    else
+      render json: {error: "Invalid username or password"}, status: :unprocessable_entity  
+    end    
+  end  
+
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'users/home'
-  #resources :users
-  resources :users, only: [:create]
+  post 'users/home'
+  post 'users/create'
+  post '/login', to: 'users#login'
+  resources :users
+  #resources :users, only: [:create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
